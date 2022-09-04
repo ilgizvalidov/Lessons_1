2b8382afa38b6d541dc0527d1c971f96d4e47ce9
Ilgiz Validov <ilgiz.validov@gmail.com>
2022-09-04 15:52:05 +0300

    сумма двух случайных целых чисел от 1 до 9

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,11 @@
-﻿int sum, number1=1, nuber2=3;
-sum = number1 + nuber2;
+﻿int sum;
+int number1 = new Random().Next(1,10);
+Console.Write("первое число:");
+Console.WriteLine(number1);
+int number2 = new Random().Next(1,10);
+Console.Write("второе число:");
+Console.WriteLine(number2);
+sum = number1 + number2;
 Console.WriteLine (sum);
 
  
